61a0010b28d2f756accd9c1623a28820788e48b6
EmilyEdna <847432003@qq.com>
2021-03-09 18:13:35 +0800

    typeceater

---
+++ Synctool/EmitFramework/TypeCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Synctool.EmitFramework
+{
+    public class TypeCreator
+    {
+        /// <summary>
+        ///  Instance
+        /// </summary>
+        public static TypeCreator Instance = new Lazy<TypeCreator>().Value;
+
+        public dynamic CreateAssembly(string AssemblyName) 
+        {
+
+            AssemblyName assembly = new AssemblyName(AssemblyName);
+
+            AssemblyBuilder builder = AssemblyBuilder.DefineDynamicAssembly(assembly, AssemblyBuilderAccess.RunAndCollect);
+
+            builder.DefineDynamicModule(AssemblyName);
+
+        }
+
+    }
+}
