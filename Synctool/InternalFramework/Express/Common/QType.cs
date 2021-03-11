2f54942816aa081df3cc13188342f44e4aa75c9b
EmilyEdna <847432003@qq.com>
2021-03-11 18:21:06 +0800

    11

---
+++ Synctool/InternalFramework/Express/Common/QType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synctool.InternalFramework.Express.Common
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public enum QType
+    {
+        /// <summary>
+        /// Like
+        /// </summary>
+        Like = 1,
+
+        /// <summary>
+        /// NotLike
+        /// </summary>
+        NotLike = 2,
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        Equals = 3,
+
+        /// <summary>
+        /// NotEquals
+        /// </summary>
+        NotEquals = 4,
+
+        /// <summary>
+        /// GreaterThan
+        /// </summary>
+        GreaterThan = 5,
+
+        /// <summary>
+        /// GreaterThanOrEqual
+        /// </summary>
+        GreaterThanOrEqual = 6,
+
+        /// <summary>
+        /// LessThan
+        /// </summary>
+        LessThan = 7,
+
+        /// <summary>
+        /// LessThanOrEqual
+        /// </summary>
+        LessThanOrEqual = 8
+    }
+}
---
+++ Synctool/InternalFramework/Express/NewExpressionVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synctool.InternalFramework.Express
+{
+    internal class NewExpressionVisitor : ExpressionVisitor
+    {
+        public ParameterExpression NewParameter { get; private set; }
+
+        public NewExpressionVisitor(ParameterExpression param)
+        {
+            NewParameter = param;
+        }
+
+        public Expression Replace(Expression exp)
+        {
+            return Visit(exp);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return NewParameter;
+        }
+    }
+}
