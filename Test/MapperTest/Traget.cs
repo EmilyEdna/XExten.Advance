b7572c57c9908a4c206f589248e756eff4f73181
EmilyEdna <847432003@qq.com>
2022-01-17 11:03:40 +0800

    升级更新

--- Test/MapperTest/Traget.cs
+++ Test/MapperTest/Traget.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test.Mapst
+namespace Test.MapperTest
 {
     public class Traget
     {
--- Test/Program.cs
+++ Test/Program.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using Test.EventTest;
-using Test.HttpTest;
+using Test.MapperTest;
 using XExten.Advance.EventFramework;
 using XExten.Advance.EventFramework.PublishEvent;
 using XExten.Advance.StaticFramework;
@@ -20,15 +20,18 @@ namespace Test
         {
             // var html = new TestClass().TestMethod();
             //var  x =  SyncStatic.HText(html);
-            EventBus.Lancher(Assembly.Load("Test"));
-            new Test1().PublishTest();
 
-            Console.ReadKey();
-            IEventPublish.Instance.PublishAsync(t => {
-                t.Payload = "OK";
-                t.EventId = "Test1";
-            });
-            Console.ReadKey();
+            //EventBus.Lancher(Assembly.Load("Test"));
+            //new Test1().PublishTest();
+
+            //Console.ReadKey();
+            //IEventPublish.Instance.PublishAsync(t => {
+            //    t.Payload = "OK";
+            //    t.EventId = "Test1";
+            //});
+            //Console.ReadKey();
+
+            new TestClass().Test();
         }
     }
 }
