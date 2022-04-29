a0f3eaadfa7df091a9275e723430edd78608032b
EmilyEdna <847432003@qq.com>
2022-04-29 14:54:21 +0800

    1

---
+++ XExten.Advance/RestHttpFramewor/RestHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XExten.Advance.RestHttpFramewor
+{
+    public class RestHttpClient:IRestHttpClient
+    {
+    }
+}
