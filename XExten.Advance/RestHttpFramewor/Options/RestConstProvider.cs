37727c09bc7fecd75b738120df6ab5a5ba31a58b
EmilyEdna <847432003@qq.com>
2022-11-25 11:37:42 +0800

    升级NUGET

--- XExten.Advance/RestHttpFramewor/Options/RestConstProvider.cs
+++ XExten.Advance/RestHttpFramewor/Options/RestConstProvider.cs
@@ -14,7 +14,7 @@ namespace XExten.Advance.RestHttpFramewor.Options
         public const string Authorization = "Authorization";
         public const string Accept = "Accept";
         public const string ContentType = "ContentType";
-        public const string AcceptLanguage = "Accept-Language";
+        public const string AcceptLanguage = "AcceptLanguage";
         public const string UserAgentApp = "Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36 Edg/100.0.4896.127";
         public const string UserAgentWindows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50";
     }
