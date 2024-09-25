cac7e80d4a5a882bf1f8a22c77ed340c4d1d08eb
EmilyEdna <847432003@qq.com>
2024-09-25 12:32:53 +0800

    修改IOC

--- XExten.Advance/IocFramework/IocDependency.cs
+++ XExten.Advance/IocFramework/IocDependency.cs
@@ -26,12 +26,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void Register<TService, TImplementation>(int Module = 2) where TService : class where TImplementation : class, TService
         {
-            if (Module == 1)
-                _Services.AddTransient<TService, TImplementation>();
-            else if (Module == 2)
-                _Services.AddSingleton<TService, TImplementation>();
-            else
-                _Services.AddScoped<TService, TImplementation>();
+            switch (Module)
+            {
+                case 1:
+                    _Services.AddTransient<TService, TImplementation>();
+                    break;
+                case 2:
+                    _Services.AddSingleton<TService, TImplementation>();
+                    break;
+                default:
+                    _Services.AddScoped<TService, TImplementation>();
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -46,12 +52,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void RegisterByNamed<TService, TImplementation>(object Name, int Module = 2) where TService : class where TImplementation : class, TService
         {
-            if (Module == 1)
-                _Services.TryAddKeyedTransient<TService, TImplementation>(Name);
-            else if (Module == 2)
-                _Services.TryAddKeyedSingleton<TService, TImplementation>(Name);
-            else
-                _Services.TryAddKeyedScoped<TService, TImplementation>(Name);
+            switch (Module)
+            {
+                case 1:
+                    _Services.TryAddKeyedTransient<TService, TImplementation>(Name);
+                    break;
+                case 2:
+                    _Services.TryAddKeyedSingleton<TService, TImplementation>(Name);
+                    break;
+                default:
+                    _Services.TryAddKeyedScoped<TService, TImplementation>(Name);
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -64,12 +76,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void Register(Type serviceType, Type implementationType, int Module = 2)
         {
-            if (Module == 1)
-                _Services.AddTransient(serviceType, implementationType);
-            else if (Module == 2)
-                _Services.AddSingleton(serviceType, implementationType);
-            else
-                _Services.AddScoped(serviceType, implementationType);
+            switch (Module)
+            {
+                case 1:
+                    _Services.AddTransient(serviceType, implementationType);
+                    break;
+                case 2:
+                    _Services.AddSingleton(serviceType, implementationType);
+                    break;
+                default:
+                    _Services.AddScoped(serviceType, implementationType);
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -83,12 +101,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void RegisterByNamed(Type serviceType, Type implementationType, object Name, int Module = 2)
         {
-            if (Module == 1)
-                _Services.TryAddKeyedTransient(serviceType, Name, implementationType);
-            else if (Module == 2)
-                _Services.TryAddKeyedSingleton(serviceType, Name, implementationType);
-            else
-                _Services.TryAddKeyedScoped(serviceType, Name, implementationType);
+            switch (Module)
+            {
+                case 1:
+                    _Services.TryAddKeyedTransient(serviceType, Name, implementationType);
+                    break;
+                case 2:
+                    _Services.TryAddKeyedSingleton(serviceType, Name, implementationType);
+                    break;
+                default:
+                    _Services.TryAddKeyedScoped(serviceType, Name, implementationType);
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -100,12 +124,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void Register(Type serviceType, int Module = 2)
         {
-            if (Module == 1)
-                _Services.AddTransient(serviceType);
-            else if (Module == 2)
-                _Services.AddSingleton(serviceType);
-            else
-                _Services.AddScoped(serviceType);
+            switch (Module)
+            {
+                case 1:
+                    _Services.AddTransient(serviceType);
+                    break;
+                case 2:
+                    _Services.AddSingleton(serviceType);
+                    break;
+                default:
+                    _Services.AddScoped(serviceType);
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -118,12 +148,18 @@ namespace XExten.Advance.IocFramework
         /// <param name="Module">1 瞬时 2 单例 3 作用域</param>
         public static void RegisterByNamed(Type serviceType, object Name, int Module = 2)
         {
-            if (Module == 1)
-                _Services.TryAddKeyedTransient(serviceType, Name);
-            else if (Module == 2)
-                _Services.TryAddKeyedSingleton(serviceType, Name);
-            else
-                _Services.TryAddKeyedScoped(serviceType, Name);
+            switch (Module)
+            {
+                case 1:
+                    _Services.TryAddKeyedTransient(serviceType, Name);
+                    break;
+                case 2:
+                    _Services.TryAddKeyedSingleton(serviceType, Name);
+                    break;
+                default:
+                    _Services.TryAddKeyedScoped(serviceType, Name);
+                    break;
+            }
 
             _Provider = _Services.BuildServiceProvider();
         }
@@ -134,10 +170,7 @@ namespace XExten.Advance.IocFramework
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static T Resolve<T>()
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetService<T>();
-        }
+            => _Provider == null ? default : _Provider.GetService<T>();
 
         /// <summary>
         /// 取出实例
@@ -146,43 +179,30 @@ namespace XExten.Advance.IocFramework
         /// <param name="Name"></param>
         /// <returns></returns>
         public static T ResolveByNamed<T>(object Name)
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetKeyedService<T>(Name);
-        }
+            => _Provider == null ? default : _Provider.GetKeyedService<T>(Name);
 
         /// <summary>
         /// 取出实例集合
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static IEnumerable<T> Resolves<T>()
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetServices<T>();
-        }
+        public static IEnumerable<T> Resolves<T>() 
+            => _Provider?.GetServices<T>();
 
         /// <summary>
         /// 取出实例集合
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static IEnumerable<T> ResolvesByNamed<T>(object Name)
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetKeyedServices<T>(Name);
-        }
-
+        public static IEnumerable<T> ResolvesByNamed<T>(object Name) 
+            => _Provider?.GetKeyedServices<T>(Name);
         /// <summary>
         /// 取出实例
         /// </summary>
         /// <param name="serviceType"></param>
         /// <returns></returns>
         public static object Resolve(Type serviceType) 
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetService(serviceType);
-        }
+            => _Provider?.GetService(serviceType);
 
         /// <summary>
         /// 取出实例
@@ -190,23 +210,16 @@ namespace XExten.Advance.IocFramework
         /// <param name="serviceType"></param>
         /// <param name="Name"></param>
         /// <returns></returns>
-        public static object ResolveByNamed(Type serviceType, object Name)
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetRequiredKeyedService(serviceType, Name);
-        }
+        public static object ResolveByNamed(Type serviceType, object Name) 
+            => _Provider?.GetRequiredKeyedService(serviceType, Name);
 
         /// <summary>
         /// 取出实例集合
         /// </summary>
         /// <param name="serviceType"></param>
         /// <returns></returns>
-        public static IEnumerable<object> Resolves(Type serviceType)
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetServices(serviceType);
-        }
-
+        public static IEnumerable<object> Resolves(Type serviceType) 
+            => _Provider?.GetServices(serviceType);
         /// <summary>
         /// 取出实例集合
         /// </summary>
@@ -214,10 +227,7 @@ namespace XExten.Advance.IocFramework
         /// <param name="Name"></param>
         /// <returns></returns>
         public static IEnumerable<object> ResolvesByNamed(Type serviceType, object Name)
-        {
-            if (_Provider == null) return default;
-            return _Provider.GetKeyedServices(serviceType, Name);
-        }
+            => _Provider?.GetKeyedServices(serviceType, Name);
 
         /// <summary>
         /// 判断实例是否存在
@@ -225,7 +235,8 @@ namespace XExten.Advance.IocFramework
         /// <typeparam name="T"></typeparam>
         /// <param name="name"></param>
         /// <returns></returns>
-        public static bool IsExistsByNamed<T>(object name) => _Provider.GetKeyedService<T>(name) != null;
+        public static bool IsExistsByNamed<T>(object name)
+            => _Provider.GetKeyedService<T>(name) != null;
 
         /// <summary>
         /// 判断实例是否存在
@@ -233,20 +244,23 @@ namespace XExten.Advance.IocFramework
         /// <param name="serviceType"></param>
         /// <param name="name"></param>
         /// <returns></returns>
-        public static bool IsExistsByNamed(Type serviceType,object name) => _Provider.GetRequiredKeyedService(serviceType, name) != null;
+        public static bool IsExistsByNamed(Type serviceType, object name)
+            => _Provider.GetRequiredKeyedService(serviceType, name) != null;
 
         /// <summary>
         /// 判断实例是否存在
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static bool IsExists<T>() => _Provider.GetService<T>() != null;
+        public static bool IsExists<T>()
+            => _Provider.GetService<T>() != null;
 
         /// <summary>
         /// 判断实例是否存在
         /// </summary>
         /// <param name="serviceType"></param>
         /// <returns></returns>
-        public static bool IsExists(Type serviceType) => _Provider.GetService(serviceType) != null;
+        public static bool IsExists(Type serviceType)
+            => _Provider.GetService(serviceType) != null;
     }
 }
